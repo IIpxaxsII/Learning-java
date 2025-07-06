1d0b20b654826f61baf63376732928f0b72d7809
IIpxrxs <bindraparas1322@gmail.com>
2025-07-06 20:03:09 +0530

    update 6 july 8pm

---
+++ typeConversion.java
@@ -0,0 +1,8 @@
+public class typeConversion {
+    public static void main(String[] args) {
+        float eg = 5;
+        System.out.println(eg);
+        int myInt = (int) 9.778787;
+        System.out.println(myInt*2); 
+    }
+}
