ed59a2a1715de8c002505f50b7e9119971fcd3fd
IIpxrxs <bindraparas1322@gmail.com>
2025-07-21 23:47:53 +0530

    initial

--- classes01.java
+++ classes01.java
@@ -10,10 +10,16 @@ class calculator{
 public class  classes01{    
 
     public static void main(String[] args) {
+       
         calculator cal = new calculator();
        System.out.println(cal.add(2,4));
+      
        calculator cal2 = new calculator();
        System.out.println(cal2.add(6,6)); 
+
+       calculator cal3 = new calculator();
+       System.out.println(cal3.add(78,6)); 
+
 
     }
     
