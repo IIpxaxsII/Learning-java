6c3e46ead61fcceed05f1692c6d6306306b18039
IIpxrxs <bindraparas1322@gmail.com>
2025-07-22 00:33:29 +0530

    22/07 0045

--- classes01.java
+++ classes01.java
@@ -1,23 +1,21 @@
-class computer{
-        public void playMusic(){
-            System.out.println("Music is being played!");
-        }
+class calculator{
+    public int add(int a, int b){
 
-        public String getPen(int cost){
-
-            return "Pen";
-        }
-
+        return a + b;
     }
+
+}
+
+    
+
 
 
 public class  classes01{    
 
     public static void main(String[] args) {
-
-        computer obj = new computer();
-        obj.playMusic();
-        System.out.println(obj.getPen(10));
+calculator obj = new calculator();
+obj.add(4, 6);
+        
     }
     }
     
