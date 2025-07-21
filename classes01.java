25a4d3ab5278fbef4a09a9e50761a2d78db1d25f
IIpxrxs <bindraparas1322@gmail.com>
2025-07-21 23:22:55 +0530

    21/07 2300

---
+++ classes01.java
@@ -0,0 +1,21 @@
+class calculator{
+        public int add(int a, int b){
+            return a + b;
+
+        }
+
+    }
+
+
+public class  classes01{    
+
+    public static void main(String[] args) {
+        calculator cal = new calculator();
+       System.out.println(cal.add(2,4));
+       calculator cal2 = new calculator();
+       System.out.println(cal2.add(6,6)); 
+
+    }
+    
+
+}
