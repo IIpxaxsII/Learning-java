32eb046c6730cec316aff5521ed0e8c1a82c459f
IIpxrxs <bindraparas1322@gmail.com>
2025-07-21 23:54:51 +0530

    21/07 2355

--- classes01.java
+++ classes01.java
@@ -1,7 +1,11 @@
-class calculator{
-        public int add(int a, int b){
-            return a + b;
+class computer{
+        public void playMusic(){
+            System.out.println("Music is being played!");
+        }
 
+        public String getPen(int cost){
+
+            return "Pen";
         }
 
     }
@@ -10,18 +14,12 @@ class calculator{
 public class  classes01{    
 
     public static void main(String[] args) {
-       
-        calculator cal = new calculator();
-       System.out.println(cal.add(2,4));
-      
-       calculator cal2 = new calculator();
-       System.out.println(cal2.add(6,6)); 
 
-       calculator cal3 = new calculator();
-       System.out.println(cal3.add(78,6)); 
-
-
+        computer obj = new computer();
+        obj.playMusic();
+        System.out.println(obj.getPen(10));
+    }
     }
     
 
-}
+
