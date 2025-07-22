f48d07ef66a2e6717ed3eddd093670029e2b2513
IIpxrxs <bindraparas1322@gmail.com>
2025-07-22 22:20:44 +0530

    22/07 2230

---
+++ array02.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class array02 {
+    
+    public static void main(String[] args) {
+        int num[] = {1,2,3,4,5};
+        for(int i=0;i<5;i++){
+         //   System.out.println(num[i]);
+        }
+    Scanner input = new Scanner(System.in);
+        int nums[][]= new int[4][4];
+        for(int i = 1; i<=3 ; i++){
+            for(int j = 1; j<=3 ; j++){
+                System.out.print("Enter " + i  + j + "value:");
+                nums[i][j] = input.nextInt();
+            }
+            
+        }
+
+        for(int i = 1; i<=3 ; i++){
+            for(int j = 1; j<=3 ; j++){
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+            
+        }
+
+
+    }
+}
--- classes01.java
+++ classes01.java
@@ -1,4 +1,6 @@
 class calculator{
+    int num;
+
     public int add(int a, int b){
 
         return a + b;
