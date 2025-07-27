f9b5ccb6e87783417857b809dcfd88269817fa97
IIpxrxs <bindraparas1322@gmail.com>
2025-07-27 21:19:08 +0530

    commits

--- inheritance05.java
+++ inheritance05.java
@@ -1,10 +1,16 @@
-
+class Calculator{
+     public int sum(int a, int b){
+    return a + b;
+     }
+    }
 
 
 
 public class inheritance05 {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.print(" sum is- ");
+       Calculator obj1 =  new Calculator();
+      System.out.println(obj1.sum(4,4));
 
 
 
