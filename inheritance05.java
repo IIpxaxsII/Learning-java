724e1838390a00362840db5791f05da96fd88172
IIpxrxs <bindraparas1322@gmail.com>
2025-07-28 23:24:43 +0530

    28.07 2324

--- inheritance05.java
+++ inheritance05.java
@@ -1,17 +1,19 @@
-class Calculator{
-     public int sum(int a, int b){
-    return a + b;
+class Parent1{
+     public void show(){
+        System.out.println("In parent1");
+
      }
+    }
+
+     class Parent2 extends Parent1{
+
     }
 
 
 
 public class inheritance05 {
     public static void main(String[] args) {
-        System.out.print(" sum is- ");
-       Calculator obj1 =  new Calculator();
-      System.out.println(obj1.sum(2,3));
-      System.out.println("hello");
+       
         
  
     }
