58cc06fe95bd19d6ecf7883b4163a8a16804a2f3
IIpxrxs <bindraparas1322@gmail.com>
2025-07-31 21:58:00 +0530

    31/07

--- Enum08.java
+++ Enum08.java
@@ -1,5 +1,14 @@
+enum Status{
+    Running, Failed, Success;
+}
+
+
+
+
 public class Enum08 {
     public static void main(String[] args) {
+        Status s = Status.Failed;
+        System.out.println(s);
         
     }
 }
